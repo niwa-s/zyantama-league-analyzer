b1000c25f292f52bc9a7b41146fe79ae56fa7419
niwa-s <souta.niwa.prog@gmail.com>
2022-07-10 02:28:49 +0900

    tailwindとかpritterとか追加

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+
+  return <h1 className="text-3xl font-bold underline text-blue-300">Hello world!</h1>;
+};
+
+export default Home;
